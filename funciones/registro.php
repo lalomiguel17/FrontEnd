5e9a2d8084862468537742fb14098853095d5b4b
lalomiguel17 <67929893+lalomiguel17@users.noreply.github.com>
2020-07-22 23:15:58 -0500

    dsa

--- funciones/registro.php
+++ funciones/registro.php
@@ -1,11 +1,39 @@
 <?php
 include("conexionbd.php");
-if($conex){
-echo "todo correcto";
 
+if(isset($_POST['registro'])){
+
+
+if(strlen($_POST['email'])>=1 && strlen($_POST['id'])>=1){
+
+$id=trim($_POST['id']);
+$nombre=trim($_POST['nombre']);
+$email=trim($_POST['email']);
+$password=trim($_POST['password']);
+$consulta = "INSERT INTO Registros(idUsuario, Nombre, Correo, Clave)VALUES('$id','$nombre','$email','$password')";
+$resultado=mysqli_query($conex,$consulta);
+
+if($resultado){
+    ?>
+    <h3 class="ok">Te has inscrito </h3>
+<?php
 }else{
+?>
 
-    echo "mal conexion"
+<h3 class="bad">ha ocurrido un error </h3>
+<?php
+
 }
+}  else{
 
 ?>
+
+<h3 class="bad">Porfavor completa los campos</h3>
+<?php
+}
+}
+?>
+
+
+
+
