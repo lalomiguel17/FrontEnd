c10cdfd74c8209535ef806c943f35862aae7cc94
lalomiguel17 <67929893+lalomiguel17@users.noreply.github.com>
2020-07-23 16:48:50 -0500

    dsadsaqw

--- funciones/registro.php
+++ funciones/registro.php
@@ -1,8 +1,38 @@
 <?php
 //include 'cn.php';
 $json=new stdClass();
-$json->id=$_POST["id"];
-$json->nombre=$_POST["nombre"];
-$json->email=$_POST["email"];
-$json->passwod=$_POST["password"];
-var_dump($json);
+$json->idUsuario=$_POST["id"];
+$json->Nombre=$_POST["nombre"];
+$json->Correo=$_POST["email"];
+$json->Clave=$_POST["password"];
+//var_dump($json);
+
+$json1=new stdClass();
+$json1->URL="http://192.168.99.100/proyectofinal/public/api/registro";
+$json1->VERBO="POST";
+$json1->JSON=json_encode($json);
+
+//apicall($json1);
+
+
+//var_dump(apicall($json1));
+header('Location: ../vista/login.html');
+
+function apicall($query){
+    $ch = curl_init();        
+    
+    $optArray = array(
+        CURLOPT_URL =>$query->URL,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_CUSTOMREQUEST=>$query->VERBO,
+        CURLOPT_POSTFIELDS=>$query->JSON,
+        CURLOPT_HTTPHEADER=>array('Content-type: application/json'),
+    );        
+    curl_setopt_array($ch, $optArray);    
+    $response=new stdClass();
+    $response->body = curl_exec($ch);        
+    $response->responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);  
+    return $response;  //
+}
