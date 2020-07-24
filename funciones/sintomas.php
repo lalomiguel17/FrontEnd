126bf1dc370e2c53e75f839fec63791c6f638408
lalomiguel17 <67929893+lalomiguel17@users.noreply.github.com>
2020-07-24 16:05:07 -0500

    gfgdsa

--- funciones/sintomas.php
+++ funciones/sintomas.php
@@ -1,12 +1,12 @@
 <?php
 //include 'cn.php';
 $json=new stdClass();
-$json->Correo=$_POST["email"];
-$json->Clave=$_POST["password"];
+//$json->Correo=$_POST["email"];
+$json->descripcion_Pregunta=$_POST["r"];
 //var_dump($json);
 
 $json1=new stdClass();
-$json1->URL="http://192.168.99.100/proyectofinal/public/api/login";
+$json1->URL="http://192.168.99.100/proyectofinal/public/api/preguntas";
 $json1->VERBO="POST";
 $json1->JSON=json_encode($json);
 
@@ -14,7 +14,7 @@ apicall($json1);
 
 //header('Location:../vista/index.html');
 
-header('Location:../vista/sintomas.html');
+header('Location:../vista/diagnostico.html');
 var_dump(apicall($json1));
 
 function apicall($query){
